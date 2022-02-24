c18742f31e388e34ee20ab4a07a72f0fbeaa721c
sanjeeva41 <72072315+sanjeeva41@users.noreply.github.com>
2022-02-24 22:36:37 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 LONG_SLEEP = 5
+SHORT_SLEEP = 2
 NIFI_XQUERY_PROCESSOR_GROUP_UUID = "07e1f50d-017f-1000-5736-ebf68e6543af"
 OWL_FILE_PATH = "D:\\Working\\PyCharm\\Output\\SM-Demonstrator-Ontology_V1.4.owl"
 ENDPOINT = "webhook"
-GITHUB_TOKEN = "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"
+GITHUB_TOKEN = "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"
 GITHUB_OWNER = "sanjeeva41"
 REPO_NAME = "MyFirst"
-HOST = "http://a19a-91-39-81-123.ngrok.io"
+HOST = "http://4608-91-39-81-123.ngrok.io"
 OWL_FILE_NAME = "SM-Demonstrator-Ontology_V1.4.owl"
